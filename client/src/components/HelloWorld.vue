1ccab992eb8e8b2579b451840da1797218073054
Vegctrp <kem.altair@gmail.com>
2019-05-05 18:30:40 +0900

    make cliant(NLP100 No.69)

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <input type="text" v-model="Name">
+    <button @click="searchName">kensaku</button>
+    {{SearchResult}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      Name: 'null',
+      SearchResult: []
+    }
+  },
+  methods: {
+    async searchName () {
+      const resp = await axios.get('http://localhost:1323?name=' + this.Name)
+      this.SearchResult = resp.data
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- main.go
+++ main.go
@@ -1,12 +1,58 @@
 package main
-import(
-	"fmt"
-	"log"
-	"net/http"
-)
 
-func main(){
-	fmt.Println("test")
-	http.Handle("/static/",http.StripPrefix("/static/",http.FileServer(http.Dir("./doc"))))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+import (
+	"fmt"
+	"net/http"
+	//"encoding/json"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
+type rating struct {
+	Count int
+	Value int
+}
+type tag struct {
+	Count int
+	Value string
+}
+type aliases struct{
+	Name string
+	Sort_name string
+}
+type artist struct {
+	ID   bson.ObjectId `bson:"_id,omitempty"`
+	Rating rating      `bson:"rating,omitempty"`
+	Name string        `bson:"name"`
+	Aliases []aliases
+	Area string        `bson:"area"`
+	Tags []tag
+}
+
+func main() {
+	// Echo instance
+	e := echo.New()
+	e.Use(middleware.CORS())
+
+	// Routes
+	e.GET("/", hello)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	search_name:=c.QueryParam("name")
+	//search_alias:=c.QueryParam("alias")
+	//search_tag:=c.QueryParam("tag")
+	fmt.Printf("name : %v\n",search_name)
+	session,_:=mgo.Dial("mongodb://localhost:27017")
+	defer session.Close()
+	db:=session.DB("database064")
+	col:=db.C("collection064")
+	var p []artist
+	col.Find(bson.M{"name":search_name}).All(&p)
+	return c.JSON(http.StatusOK, p)
 }
